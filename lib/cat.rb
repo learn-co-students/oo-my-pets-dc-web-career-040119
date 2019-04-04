5396a3befe92f94c449f0454e41589a1c4f2bdc3
marior9 <mariorodriguezan@gmail.com>
2019-04-04 16:24:11 -0400

    done

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
   # code goes here
+    @@all=[]
+  attr_reader :name, :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def mood=(mood)
+    @mood = mood
+
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,16 @@
 class Fish
   # code goes here
+  @@all = []
+  attr_reader :name, :mood
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+  def mood=(mood)
+    @mood = mood
+  end
+  def self.all
+    @@all
+  end
 end
